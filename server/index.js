63aa79b9026a2f0f657414f203bb71afe9909c41
Achraf <117481103+8ird8@users.noreply.github.com>
2024-04-29 00:03:25 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://you-fit-rouge.vercel.app",
   methods: ["POST", "GET", "DELETE", "PUT"],
   credentials: true,
 };
