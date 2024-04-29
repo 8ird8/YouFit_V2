08d9d3e17a1d63c943dbf20b9144a7fa7394f4be
Achraf <117481103+8ird8@users.noreply.github.com>
2024-04-29 20:10:54 +0100

    Update workoutDetails.tsx

--- client/src/workoutDetails.tsx
+++ client/src/workoutDetails.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect,useContext } from "react";
 import axios from "axios";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import WorkoutExCard from "./WorkoutExCard";
