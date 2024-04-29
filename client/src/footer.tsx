3a12e5c23c663c1ed990903517d81c9ae1ecad98
Achraf <117481103+8ird8@users.noreply.github.com>
2024-04-30 00:31:28 +0100

    Update footer.tsx

--- client/src/footer.tsx
+++ client/src/footer.tsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <div>
+    <div className= "mt-20">
       <footer className="footer section" id="footer">
         <div className="container">
           <div className="footer__container">
